deae23b714fd346a60cadac2bb095addf7dc38a1
Takács Marcell <tmarcell20012001@gmail.com>
2024-05-10 14:04:55 +0200

    Final changes

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ import neat
 SCREEN_WIDTH = 1244
 SCREEN_HEIGHT = 1016
 SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
 TRACK = pygame.image.load(os.path.join("Assets", "track.png"))
 
 
